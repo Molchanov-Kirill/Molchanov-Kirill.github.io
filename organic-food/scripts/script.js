0f91bf9035a6f960b74318828b2cf3f48a63be1e
Molchanov-Kirill <63114046+Molchanov-Kirill@users.noreply.github.com>
2020-08-30 14:17:28 +0300

    Add files via upload

---
+++ organic-food/scripts/script.js
@@ -0,0 +1,33 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.reviews__pagination',
+    },
+  });
+  
+  DG.then(function() {
+    DG.map('map', {
+        center: [54.98, 82.89],
+        zoom: 13
+    });
+  });
+
+  const burgerButton = document.querySelector('.header__burger');
+  const mobileMenu = document.querySelector('.mobile');
+  const mobileClose = document.querySelector('.mobile__close');
+
+  burgerButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    mobileMenu.classList.add('mobile--active');
+    document.body.style.overflowY = 'hidden';
+  });
+
+
+  mobileClose.addEventListener('click', (e) => {
+    e.preventDefault();
+    mobileMenu.classList.remove('mobile--active');
+    document.body.style.overflowY = '';
+  });
+});
+
